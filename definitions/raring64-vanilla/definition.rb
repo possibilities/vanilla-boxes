4f86b23ad665fd3816a9d7d26245a49c26d74bec
Mike Bannister <notimpossiblemike@gmail.com>
2013-11-23 22:58:07 -0800

    Make them bigger!

--- definitions/raring64-vanilla/definition.rb
+++ definitions/raring64-vanilla/definition.rb
@@ -1,7 +1,7 @@
 Veewee::Session.declare({
   :cpu_count => '1',
   :memory_size=> '2048',
-  :disk_size => '10140',
+  :disk_size => '40960',
   :disk_format => 'VDI',
   :hostiocache => 'off',
   :os_type_id => 'Ubuntu_64',
